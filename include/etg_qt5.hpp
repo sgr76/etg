2b080903d3e65b95c6a03abb7b5b6096f22c5f05
Sebastian Groth <sgroth@gmx.de>
2020-04-04 23:02:28 +0200

    uncrustify

--- include/etg_qt5.hpp
+++ include/etg_qt5.hpp
@@ -7,13 +7,17 @@
 
 
 template<typename T>
+
 struct etg
 {
-    static T cast (int) = delete;
-    static T cast (const QString&) = delete;
+    static T cast (int)             = delete;
+    static T cast (const QString &) = delete;
+
     static const QString debugSymbol(T) = delete;
-    static const QString token(T) = delete;
+    static const QString token(T)       = delete;
     static const QString translation(T) = delete;
-    static typename std::set<T>::const_iterator begin() = delete;
-    static typename std::set<T>::const_iterator end() = delete;
+
+    static typename std::set<T>::const_iterator begin () = delete;
+
+    static typename std::set<T>::const_iterator end () = delete;
 };
